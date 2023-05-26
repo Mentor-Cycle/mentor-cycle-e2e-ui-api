b0c2cd9bd3a21689de9ed5ae0caa879d4da41b0f
gscode <adm.guilhermedesousa@gmail.com>
2023-05-26 01:26:51 -0300

    start no projeto de automação

--- cypress/e2e/ui/login.cy.js
+++ cypress/e2e/ui/login.cy.js
@@ -2,7 +2,7 @@ describe('Login', () => {
   it('Realizando login com sucesso', () => {
     cy.visit('/')
 
-    cy.get('input[name="email"]').type('ti.gscode+mentorado1@gmail.com')
+    cy.get('input[name="email"]').type('ti.gscode+mentorado@gmail.com')
     cy.get('input[name="password"]').type('#Pwd1234')
     cy.contains('button', 'Entrar').click()
   })
